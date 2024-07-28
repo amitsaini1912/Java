44f2d93a36bab4d0d0b230194da100cd97aaf7eb
amitsaini1912 <amitkumarsainig9@gmail.com>
2024-07-28 12:20:43 +0530

    prime no. program added in loops file

--- src/loops.java
+++ src/loops.java
@@ -3,6 +3,28 @@ import java.util.Scanner;
 
 public class loops {
 
+    //Check whether a number prime or not
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a No. : ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+        for(int i=2; i<=Math.sqrt(n); i++){ //for(int i=2; i<n; i++)
+           if(n%i == 0 ){
+               isPrime = false;
+           }
+        }
+
+        if(isPrime == true){
+            System.out.println("No. is prime");
+        }
+        else {
+            System.out.println("No. is not prime");
+        }
+    }
+
+
     //Find reverse of a number (While Loop)
     /*public static void main(String[] args ){
         int n = 10899;
@@ -13,6 +35,19 @@ public class loops {
             n = n/10;
         }
         System.out.println("Reverse No. is : " + rev);
+    }*/
+    /* Reverse a user given no. using do while loop
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No. that you want to reverse : ");
+        int n = sc.nextInt();
+        int rev = 0;
+        do{
+            int lastdigit = n%10;
+            rev = (rev*10)+lastdigit;
+            n = n/10;
+        } while (n>0);
+        System.out.println("Reversed No. : " +rev);
     }*/
 
 
@@ -30,18 +65,19 @@ public class loops {
         }while (true);
      }*/
 
-    //Check whether a number prime or not
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-         do{
-             System.out.print("Enter a number: ");
-             int num = sc.nextInt();
-             if ((num % 2) != 0){
-                 System.out.println("no. is not prime");
-                 continue;
-             }
-             System.out.println("No. is Prime.");
-         }while (true);
-    }
+    //Print a square pattern
+    /*public static void main(String[] args){
+
+        for(int i=1; i<=5; i++){
+                System.out.println("*****");
+        }
+        int line = 1;
+        while (line <= 4){
+            System.out.println("****");
+            line++;
+        }
+     }*/
+
+
 
 }
